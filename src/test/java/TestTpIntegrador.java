de68aa3c1dbc453b85f6244ed4c0329f9a11ad01
leonnelc <78927152+leonnelc@users.noreply.github.com>
2023-04-24 23:56:56 -0300

    Mejorados tests

--- src/test/java/TestTpIntegrador.java
+++ src/test/java/TestTpIntegrador.java
@@ -1,7 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 public class TestTpIntegrador {
-    @org.junit.jupiter.api.Test
-    public void test1(){
-        //
-        tpIntegrador.main(new String[]{"C:/Users/Leo/Documents/UtnCurso/integrador/tpIntegrador2/target/test-classes/configDB1.props","src/test/resources/config1.props"});
+    // uno de los tests tiene que estar comentado para que el otro funcione
+    /*
+    @Test
+    public void testSQL(){
+        System.out.println("Ejecutando testSQL");
+        tpIntegrador.main(new String[]{"src/test/resources/configDBSQL.props","src/test/resources/config1.props"});
+
+    }
+     */
+    @Test
+    public void testCSV(){
+        System.out.println("Ejecutando testCSV");
+        tpIntegrador.main(new String[]{"C:/Users/Leo/Documents/UtnCurso/integrador/tpIntegrador2/src/test/resources/ConfigDBCSV.props", "C:/Users/Leo/Documents/UtnCurso/integrador/tpIntegrador2/src/test/resources/config1.props"});
     }
 }
